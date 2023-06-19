d0e876b4b3d03dd52fe853ebe2da8f677464a9e1
kristijan <jankuloski.k@gmail.com>
2023-06-19 20:35:20 +0200

    in the begining

---
+++ BurgerApp.Domain/Enums/PaymentMethod.cs
@@ -0,0 +1,8 @@
+﻿namespace BurgerApp.Domain.Enums
+{
+    public enum PaymentMethod
+    {
+        Cash = 1,
+        Card
+    }
+}
---
+++ BurgerApp.Domain/Models/Burger.cs
@@ -0,0 +1,8 @@
+﻿namespace BurgerApp.Domain.Models
+{
+    public class Burger : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public double Price { get; set; }
+    }
+}
